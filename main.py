a37a8545fd09966f0480c51cb2bc1b6d1177b78c
cclally <cclally@hotmail.com>
2025-01-10 21:13:59 -0600

    instantiated updatable and drawable group containers to store all update and draw containers to loop through. This will keep the game loop clean from multiple update and draw calls.

--- main.py
+++ main.py
@@ -11,19 +11,34 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        
+    
+        # player.draw(screen)
+        #   draw all items in the pygame group "drawable"
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
 
-        #limit framerate to 60fps
+        # limit framerate to 60fps
         dt = clock.tick(60) / 1000
-        player.update(dt)
+
+        # player.update(dt)
+        #  update all item in the group "updatable"
+        for item in updatable:
+            item.update(dt)
 
 if __name__ == "__main__":
     main()
